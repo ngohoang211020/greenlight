bafa861444d3e65903d6bce8d89bfb3d0ba449d4
BANVIENCORP\hoang.ngo <hoang.ngo@banvien.com.vn>
2024-01-30 00:32:34 +0700

    Handler Race limit per ip address utilizing package golang.org/x/time to set race limit

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/time/rate"
+	"net"
 	"net/http"
+	"sync"
+	"time"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
@@ -14,5 +18,59 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 			}
 		}()
 		next.ServeHTTP(writer, request)
+	})
+}
+
+func (app *application) rateLimit(next http.Handler) http.Handler {
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+
+	// Launch a background goroutine which removes old entries from the clients map once
+	// every minute.
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+			mu.Lock()
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			mu.Unlock()
+		}
+	}()
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
+		// Lock the mutex to prevent this code from being executed concurrently.
+		mu.Lock()
+
+		if _, found := clients[ip]; !found {
+			clients[ip] = &client{
+				limiter: rate.NewLimiter(rate.Limit(app.config.limiter.rps), app.config.limiter.burst),
+			}
+		}
+		clients[ip].lastSeen = time.Now()
+
+		if !clients[ip].limiter.Allow() {
+			mu.Unlock()
+			app.rateLimitExceededResponse(w, r)
+			return
+		}
+		// Very importantly, unlock the mutex before calling the next handler in the
+		// chain. Notice that we DON'T use defer to unlock the mutex, as that would mean
+		// that the mutex isn't unlocked until all the handlers downstream of this
+		// middleware have also returned.
+		mu.Unlock()
+		next.ServeHTTP(w, r)
 	})
 }
--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.20
 
 require github.com/julienschmidt/httprouter v1.3.0
 
-require github.com/lib/pq v1.10.0 // indirect
+require (
+	github.com/lib/pq v1.10.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
+)
